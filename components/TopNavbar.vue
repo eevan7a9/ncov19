c45475b57e61e9403163e86340f576e507e3bda9
eevan7a9 <e7a9@protonmail.com>
2020-03-26 08:59:51 +0900

    added top navbar

---
+++ components/TopNavbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="danger">
+      <b-navbar-brand href="#">Ncov19</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/" class="text-capitalize">home</b-nav-item>
+          <b-nav-item to="/about" class="text-capitalize">about</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+          <!--
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown> -->
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopNavbar'
+}
+</script>
+
+<style scoped>
+a.nav-link.nuxt-link-exact-active {
+  color: rgb(255, 255, 255);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
+    <TopNavbar />
     <nuxt />
   </div>
 </template>
 
+<script>
+import TopNavbar from '@/components/TopNavbar'
+export default {
+  components: {
+    TopNavbar
+  }
+}
+</script>
+
 <style>
+@import '~/assets/style/custom-style.css';
+
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -23,33 +35,7 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.text-green {
+  color: green;
 }
 </style>
