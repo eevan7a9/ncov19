524bae2d209d26edac5c753b250844d9f1b45bc7
eevan7a9 <e7a9@protonmail.com>
2020-03-26 12:28:58 +0900

    added toggle nav

--- components/leftSidebar/LeftSidebar.vue
+++ components/leftSidebar/LeftSidebar.vue
@@ -1,14 +1,29 @@
 <template lang="pug">
   div( class="left-sidebar-wrapepr bg-dark text-light" v-bind:class="leftSidebar ? 'show' : ''")
-    h1.m-0 Hello World
-    p {{leftSidebar}}
-    button( @click="toggleLeftSidebar") toggle Navbar
+    div.d-flex.justify-content-end.align-items-center.text-light
+      h6.m-0
+        BIconArrowBarLeft(class="m-2 mr-3 font-weight-bold" font-scale="2"
+          @click="toggleLeftSidebar"
+          v-if="leftSidebar"
+        )
+        BIconBoxArrowInRight(class="m-2 mr-3 font-weight-bold" font-scale="2"
+          @click="toggleLeftSidebar"
+          v-if="!leftSidebar"
+        )
+    div.topnav-left-sidebar.bg-danger.m-0(v-show="leftSidebar")
+      h5 header
 
 </template>
 
 <script>
+import { BIcon, BIconArrowBarLeft, BIconBoxArrowInRight } from 'bootstrap-vue'
 import { mapGetters, mapActions } from 'vuex'
 export default {
+  components: {
+    BIcon,
+    BIconArrowBarLeft,
+    BIconBoxArrowInRight
+  },
   computed: mapGetters('leftSidebar', ['leftSidebar']),
   methods: {
     ...mapActions('leftSidebar', ['toggleLeftSidebar'])
@@ -19,9 +34,9 @@ export default {
 <style lang="scss" scoped>
 .left-sidebar-wrapepr {
   width: 50px;
+  transition: ease-in-out 0.3s;
   &.show {
     width: 300px;
-    transition: 0.4s;
   }
 }
 </style>
