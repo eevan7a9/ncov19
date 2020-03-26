b2d24fbce082ec45bcd19f22e1be26c48f35f4f8
eevan7a9 <e7a9@protonmail.com>
2020-03-26 12:29:07 +0900

    added in container

--- components/TopNavbar.vue
+++ components/TopNavbar.vue
@@ -2,28 +2,27 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="danger">
       <b-navbar-brand href="#">Ncov19</b-navbar-brand>
-
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <div class="container">
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/" class="text-capitalize">home</b-nav-item>
+            <b-nav-item to="/about" class="text-capitalize">about</b-nav-item>
+          </b-navbar-nav>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/" class="text-capitalize">home</b-nav-item>
-          <b-nav-item to="/about" class="text-capitalize">about</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input
-              size="sm"
-              class="mr-sm-2"
-              placeholder="Search"
-            ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
-              >Search</b-button
-            >
-          </b-nav-form>
-          <!--
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form>
+            <!--
           <b-nav-item-dropdown text="Lang" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
             <b-dropdown-item href="#">ES</b-dropdown-item>
@@ -38,8 +37,9 @@
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
           </b-nav-item-dropdown> -->
-        </b-navbar-nav>
-      </b-collapse>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
     </b-navbar>
   </div>
 </template>
