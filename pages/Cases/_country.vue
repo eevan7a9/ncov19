1475962d1bd912279706493a398aa41f9159ce06
eevan7a9 <e7a9@protonmail.com>
2020-03-27 14:28:43 +0900

    added route page for cases cuntry

---
+++ pages/Cases/_country.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    h1 {{country}}
+
+</template>
+
+<script>
+export default {
+  layout: 'dashboard',
+  data() {
+    return {
+      country: this.$route.params.country
+    }
+  }
+}
+</script>
+
+<style></style>
