ee033941638d540d542e45c4e5e718cb9d263f80
eevan7a9 <e7a9@protonmail.com>
2020-03-27 14:28:14 +0900

    added layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
-<template>
-  <div class="bg-info">
-    <h1>Home</h1>
-  </div>
+<template lang="pug">
+  div.d-flex.flex-column
+    div.row.m-0
+      div.col-4.bg-success
+        h1 Col -1
+      div.col-4.bg-secondary
+        h1 Col -2
+      div.col-4.bg-primary
+        h1 Col -3
+    div.bg-warning.flex-grow-1
+      h1 main cont
 </template>
 
 <script>
