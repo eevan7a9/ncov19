975ec7aa74ed10e242c4345acc0c7c744abd1865
eevan7a9 <e7a9@protonmail.com>
2020-03-29 18:03:20 +0900

    added & configure  vue leaflet

--- components/countries/CountriesMap.vue
+++ components/countries/CountriesMap.vue
@@ -1,11 +1,25 @@
 <template lang="pug">
-  div#mapContainer.bg-info.h-100
-    h1 MyMap
+  div#mapContainer.h-100.w-100
+    client-only
+      l-map(:zoom=13 :center="[55.9464418,8.1277591]")
+        l-tile-layer(url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+        l-marker(:lat-lng="[55.9464418,8.1277591]")
+
 </template>
 
 <script>
 export default {
-  name: 'CountriesMap'
+  name: 'CountriesMap',
+  data() {
+    return {
+      domReady: false
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.domReady = true
+    }, 2000)
+  }
 }
 </script>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,10 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{ src: '@/plugins/vue-dragscroll.js', ssr: false }],
+  plugins: [
+    { src: '@/plugins/vue-dragscroll.js', ssr: false },
+    { src: '~plugins/leaflet.js', ssr: false }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
