0bd8b03ff2c8d270bd79c088530b358f0c389b58
eevan7a9 <e7a9@protonmail.com>
2020-03-29 23:57:36 +0900

    enable fetch multiple marker base on data

--- components/countries/CountriesMap.vue
+++ components/countries/CountriesMap.vue
@@ -1,24 +1,45 @@
 <template lang="pug">
   div#mapContainer.h-100.w-100
-    client-only
-      l-map(:zoom=13 :center="[55.9464418,8.1277591]")
-        l-tile-layer(url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-        l-marker(:lat-lng="[55.9464418,8.1277591]")
+    //- p {{getCountriesCases.slice(0,4)}}
+    client-only(v-if="domReady")
+      l-map(:zoom="map.zoom" :center="map.center")
+        l-tile-layer(:url="tile.url" :attribution="tile.attribution")
+        l-marker(v-for="(marker, index) in getCountriesCases" :key="index" :lat-lng="marker.latLang")
+
 
 </template>
 
 <script>
+import countriesData from 'assets/dummy-data.json'
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'CountriesMap',
   data() {
     return {
-      domReady: false
+      map: {
+        zoom: 4,
+        center: [55.9464418, 8.1277591]
+      },
+      tile: {
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      },
+      markers: [
+        [55.1464418, 8.9277591],
+        [55.9464418, 8.1274391]
+      ],
+      domReady: true
     }
   },
+  computed: mapGetters('countries', ['getCountriesCases']),
   mounted() {
-    setTimeout(() => {
-      this.domReady = true
-    }, 2000)
+    this.getCountriesInfo(countriesData.Countries)
+  },
+  created() {},
+  methods: {
+    ...mapActions('countries', ['getCountriesInfo']),
+    getCircleMarkers() {}
   }
 }
 </script>
--- store/countries.js
+++ store/countries.js
@@ -1,13 +1,16 @@
 import countriesList from '@/assets/countries-list.json'
+import countriesInfo from '@/assets/countries-info.json' // lat-long, country-code etc.
 
 export const state = () => {
   return {
-    countriesList: JSON.parse(JSON.stringify(countriesList.countries))
+    countriesList: JSON.parse(JSON.stringify(countriesList.countries)),
+    countriesCases: []
   }
 }
 
 export const getters = {
-  countries: (state) => state.countriesList
+  countries: (state) => state.countriesList,
+  getCountriesCases: (state) => state.countriesCases
 }
 
 export const actions = {
@@ -20,9 +23,29 @@ export const actions = {
       )
     }
     commit('setCountries', countries)
+  },
+  getCountriesInfo: ({ commit }, countriesCases) => {
+    let countries = countriesCases.map((country) => {
+      const countryName = country.Country.trim().toLowerCase()
+      if (countryName) {
+        const foundCountry = countriesInfo.countries.find(
+          (data) => data.name.trim().toLowerCase() === countryName
+        )
+        if (foundCountry) {
+          country.latLang = foundCountry.latlng
+          return country
+        }
+      }
+    })
+    countries = countries
+      .sort((a, b) => b.TotalConfirmed - a.TotalConfirmed)
+      .filter((country) => country !== undefined)
+    // console.log(countries)
+    commit('setCasesSummary', countries)
   }
 }
 
 export const mutations = {
-  setCountries: (state, countries) => (state.countriesList = countries)
+  setCountries: (state, countries) => (state.countriesList = countries),
+  setCasesSummary: (state, countries) => (state.countriesCases = countries)
 }
