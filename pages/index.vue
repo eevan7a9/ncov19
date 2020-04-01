13ce2292aeb864fcc18d3ae9bd5dc4bd8fac5ffc
eevan7a9 <e7a9@protonmail.com>
2020-04-01 16:56:08 +0900

    enable show/hide topPanel

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
   div.d-flex.flex-column(v-if="initDom")
-    div#topPanels.row.m-0.bg-dark
+    div#topPanels.row.m-0.bg-dark(:class="showTopPanel ? 'show': 'hide'")
       div.all-cases.col-4.p-0.h-100
         AllCases
       div.top-cases.col-4.p-0.h-100
         TopCases
       div.global-numbers.col-4.p-0.h-100
         GlobalNumbers
-    button.slider-button.btn.btn-dark.d-flex.justify-content-center.align-items-center
+    button.slider-button.p-0.btn.btn-dark.d-flex.justify-content-center.align-items-center(
+      @click="showTopPanel=!showTopPanel"
+    )
       small
-         BIconTriangleFill
+         BIconTriangleFill(:flip-v="!showTopPanel")
     div.flex-grow-1
-      CountriesMap
+      CountriesMap#icon(:key="showTopPanel")
 </template>
 
 <script>
@@ -35,7 +37,8 @@ export default {
   data() {
     return {
       casesSummary: [],
-      initDom: false
+      initDom: false,
+      showTopPanel: true
     }
   },
   computed: mapGetters(['getSummaryCases']),
@@ -51,17 +54,31 @@ export default {
 
 <style lang="scss" scoped>
 #topPanels {
-  height: 300px;
   overflow: hidden;
   // background: aliceblue;
+  &.show {
+    height: 300px;
+  }
+  &.hide {
+    height: 0;
+  }
   div {
     border: 1px solid rgb(121, 121, 121);
   }
 }
 .slider-button {
-  height: 15px;
+  height: 25px;
+  color: #c4c4c4;
+  border-radius: 0;
+  &:hover:before {
+    transform: scale(1.2);
+    box-shadow: 0 0 15px #ffffff;
+    filter: blur(3px);
+  }
   &:hover {
-    height: 25px;
+    color: aliceblue;
+    box-shadow: 0 0 15px #7e7976;
+    text-shadow: 0 0 15px #fffcfa;
   }
 }
 </style>
