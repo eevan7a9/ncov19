295fb5e62c087df985b96fe1dd418032272bd15d
eevan7a9 <e7a9@protonmail.com>
2020-04-01 15:15:03 +0900

    remove searchbar

--- components/TopNavbar.vue
+++ components/TopNavbar.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="danger">
-      <b-navbar-brand href="#">Ncov19</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <div class="container">
+        <b-navbar-brand href="#">Ncov19</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item to="/" class="text-capitalize">home</b-nav-item>
-            <b-nav-item to="/about" class="text-capitalize">about</b-nav-item>
+            <!-- <b-nav-item to="/item" class="text-capitalize">item</b-nav-item> -->
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-form>
+            <b-navbar-nav>
+              <b-nav-item to="/" class="text-capitalize">home</b-nav-item>
+              <b-nav-item to="/about" class="text-capitalize">about</b-nav-item>
+            </b-navbar-nav>
+            <!-- <b-nav-form>
               <b-form-input
                 size="sm"
                 class="mr-sm-2"
@@ -21,7 +25,7 @@
               <b-button size="sm" class="my-2 my-sm-0" type="submit"
                 >Search</b-button
               >
-            </b-nav-form>
+            </b-nav-form> -->
             <!--
           <b-nav-item-dropdown text="Lang" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
