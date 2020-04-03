f82c8e75fcc6c688eeb427b2c98d845a0201f26c
eevan7a9 <e7a9@protonmail.com>
2020-04-04 01:30:48 +0900

    added cases country layout

--- pages/Cases/_country.vue
+++ pages/Cases/_country.vue
@@ -1,18 +1,65 @@
 <template lang="pug">
-  div
-    h1 {{country}}
+  div.d-flex.flex-column(v-if="initDom")
+    div#topPanels.row.m-0.bg-dark(:class="showTopPanel ? 'show': 'hide'")
+      div.country-cases.col-sm-6.p-0.h-100
+
+      div.country-numbers.col-sm-6.p-0.h-100
+
+    button.slider-button.p-0.btn.btn-dark.d-flex.justify-content-center.align-items-center(
+      @click="showTopPanel=!showTopPanel"
+    )
+      small
+         BIconTriangleFill(:flip-v="!showTopPanel")
+    div.flex-grow-1
+      h1   {{country}}
 
 </template>
 
 <script>
+import { BIcon, BIconTriangleFill } from 'bootstrap-vue'
 export default {
   layout: 'dashboard',
+  components: {
+    BIcon,
+    BIconTriangleFill
+  },
   data() {
     return {
+      initDom: true,
+      showTopPanel: true,
       country: this.$route.params.country
     }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+#topPanels {
+  overflow: hidden;
+  // background: aliceblue;
+  &.show {
+    height: 300px;
+  }
+  &.hide {
+    height: 0;
+  }
+  div {
+    border: 1px solid rgb(121, 121, 121);
+  }
+}
+.slider-button {
+  height: 25px;
+  color: #c4c4c4;
+  border-radius: 0;
+  &:hover:before {
+    transform: scale(1.2);
+    box-shadow: 0 0 15px #ffffff;
+    filter: blur(3px);
+  }
+  &:hover {
+    color: aliceblue;
+    box-shadow: 0 0 15px #7e7976;
+    text-shadow: 0 0 15px #fffcfa;
+  }
+}
+</style>
