a3871f9eafdec1285d8f42f6ff0bc09f4dffa88c
eevan7a9 <e7a9@protonmail.com>
2020-04-05 19:39:40 +0900

    moved fetching api country data

--- components/countries/CountryTimeline.vue
+++ components/countries/CountryTimeline.vue
@@ -1,27 +1,20 @@
 <template lang="pug">
-  div#countryTimeline.h-100(v-if="initDom")
+  div#countryTimeline.h-100
+    p {{getCountryDetailedCases}}
     div.chart-container(v-if="datacollection")
       LineChart.country-timeline(:chart-data="datacollection" :options="options")
 </template>
 
 <script>
-import { mapGetters, mapActions, mapMutations } from 'vuex'
+import { mapGetters } from 'vuex'
 import LineChart from '@/components/charts/LineChart'
 export default {
   name: 'CountryTimeline',
   components: {
     LineChart
   },
-  props: {
-    country: {
-      require: true,
-      type: String,
-      default: () => 'us'
-    }
-  },
   data() {
     return {
-      initDom: false,
       datacollection: null,
       options: {
         responsive: true,
@@ -40,53 +33,8 @@ export default {
   },
   mounted() {
     this.fillData()
-
-    const storedCases = JSON.parse(localStorage.getItem('DetailedCases'))
-
-    if (storedCases) {
-      const storedSeconds =
-        (new Date().getTime() - storedCases.StoredTime) / 1000
-      // if stored data is more than one hour we fetch a new one
-      if (storedSeconds > 3600 || storedCases.Slug !== this.country) {
-        localStorage.removeItem('DetailedCases')
-        this.getDetailedCases().then(() => {
-          this.initDom = true
-          // console.log('fetch fresh - time expired or different slug')
-        })
-      } else {
-        // if the stored data is lesss than 1hr old we fetch it
-        // console.log('fetch stored')
-        this.SET_COUNTRY_DETAILS(storedCases.Cases)
-        this.initDom = true
-      }
-    } else {
-      this.getDetailedCases().then(() => {
-        this.initDom = true
-        // console.log('fetch fresh - first')
-      })
-    }
   },
   methods: {
-    ...mapActions('countries', ['fetchCountryDetailedCases']),
-    ...mapMutations('countries', ['SET_COUNTRY_DETAILS']),
-
-    async getDetailedCases() {
-      const cases = await this.fetchCountryDetailedCases({
-        countrySlug: this.country,
-        status: 'confirmed'
-      })
-      // we store the data to localstorage
-      localStorage.setItem(
-        'DetailedCases',
-        JSON.stringify({
-          // cases of the country
-          Cases: cases,
-          Slug: this.country,
-          // holds time item was stored
-          StoredTime: new Date().getTime()
-        })
-      )
-    },
     fillData() {
       this.datacollection = {
         datasets: [
--- pages/Cases/_country.vue
+++ pages/Cases/_country.vue
@@ -11,12 +11,13 @@
       small
          BIconTriangleFill(:flip-v="!showTopPanel")
     div.flex-grow-1
-      CountryTimeline(:country="country")
+      CountryTimeline
 
 </template>
 
 <script>
 import { BIcon, BIconTriangleFill } from 'bootstrap-vue'
+import { mapActions, mapMutations } from 'vuex'
 import CountryTimeline from '@/components/countries/CountryTimeline'
 export default {
   layout: 'dashboard',
@@ -29,7 +30,53 @@ export default {
     return {
       initDom: true,
       showTopPanel: true,
-      country: this.$route.params.country
+      country: this.$route.params.country,
+      cases: []
+    }
+  },
+  mounted() {
+    const storedCases = JSON.parse(localStorage.getItem('DetailedCases'))
+
+    if (storedCases) {
+      const storedSeconds =
+        (new Date().getTime() - storedCases.StoredTime) / 1000
+      // if stored data is more than one hour we fetch a new one
+      if (storedSeconds > 3600 || storedCases.Slug !== this.country) {
+        localStorage.removeItem('DetailedCases')
+        this.getDetailedCases().then(() => {
+          this.initDom = true
+          // console.log('fetch fresh - time expired or different slug')
+        })
+      } else {
+        // if the stored data is lesss than 1hr old we fetch it
+        // console.log('fetch stored')
+        this.SET_COUNTRY_DETAILS(storedCases.Cases)
+        this.initDom = true
+      }
+    } else {
+      this.getDetailedCases().then(() => {
+        this.initDom = true
+        // console.log('fetch fresh - first')
+      })
+    }
+  },
+  methods: {
+    ...mapActions('countries', ['fetchCountryDetailedCases']),
+    ...mapMutations('countries', ['SET_COUNTRY_DETAILS']),
+
+    async getDetailedCases() {
+      const cases = await this.fetchCountryDetailedCases(this.country)
+      // we store the data to localstorage
+      localStorage.setItem(
+        'DetailedCases',
+        JSON.stringify({
+          // cases of the country
+          Cases: cases,
+          Slug: this.country,
+          // holds time item was stored
+          StoredTime: new Date().getTime()
+        })
+      )
     }
   }
 }
