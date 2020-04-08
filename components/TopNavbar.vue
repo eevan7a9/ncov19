c19701f952feb245f0fe01a3eb49c4660a4a0099
eevan7a9 <e7a9@protonmail.com>
2020-04-09 02:53:59 +0900

    enable searchbar and search page

--- components/TopNavbar.vue
+++ components/TopNavbar.vue
@@ -12,25 +12,18 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <!-- <b-nav-item to="/item" class="text-capitalize">item</b-nav-item> -->
+          <b-nav-item to="/" class="text-capitalize">home</b-nav-item>
+          <b-nav-item to="/about" class="text-capitalize">about</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-navbar-nav>
-            <b-nav-item to="/" class="text-capitalize">home</b-nav-item>
-            <b-nav-item to="/about" class="text-capitalize">about</b-nav-item>
+            <!-- <b-nav-item to="/item" class="text-capitalize">item</b-nav-item> -->
           </b-navbar-nav>
-          <!-- <b-nav-form>
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-              ></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                >Search</b-button
-              >
-            </b-nav-form> -->
+          <!-- Searchbar starts -->
+          <SearchBar />
+          <!-- Searchbar ends -->
           <!--
           <b-nav-item-dropdown text="Lang" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
@@ -53,8 +46,12 @@
 </template>
 
 <script>
+import SearchBar from '@/components/SearchBar'
 export default {
-  name: 'TopNavbar'
+  name: 'TopNavbar',
+  components: {
+    SearchBar
+  }
 }
 </script>
 
---
+++ pages/Search/_content.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div.container
+    h1 {{ $route.params.content }}
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
