096d759323578d1828086c9f3603d740f244a527
eevan7a9 <e7a9@protonmail.com>
2020-04-11 18:46:50 +0900

    fixed undefined error

--- components/casesVisualized/CountryCases.vue
+++ components/casesVisualized/CountryCases.vue
@@ -40,30 +40,60 @@ export default {
       }
     }
   },
-  computed: mapGetters('countries', ['getCountriesCases']),
+  computed: mapGetters('countries', [
+    'getCountriesCases',
+    'getCountryDetailedCases'
+  ]),
+  watch: {
+    getCountryDetailedCases: {
+      // eslint-disable-next-line object-shorthand
+      handler: function(newVal) {
+        this.fillData()
+      },
+      deep: true
+    }
+  },
   mounted() {
-    const countryDetails = this.getCountriesCases.find(
-      (c) => c.name.toLowerCase() === this.country.toLowerCase()
-    )
-    const active =
-      countryDetails.TotalConfirmed -
-      (countryDetails.TotalDeaths + countryDetails.TotalRecovered)
-    this.fillData({
-      active,
-      death: countryDetails.TotalDeaths,
-      treated: countryDetails.TotalRecovered
-    })
-    this.options.title.text = `Total Number of confirmed cases: ${countryDetails.TotalConfirmed}`
+    this.fillData()
   },
   methods: {
-    fillData({ active, death, treated }) {
+    fillData() {
+      const doughnut = {}
+
+      const countryDetails = this.getCountriesCases.find(
+        (country) => country.name.toLowerCase() === this.country.toLowerCase()
+      )
+
+      if (countryDetails) {
+        // if country exists in countries cases
+        doughnut.active =
+          countryDetails.TotalConfirmed -
+          (countryDetails.TotalDeaths + countryDetails.TotalRecovered)
+        doughnut.deaths = countryDetails.TotalDeaths
+        doughnut.recovered = countryDetails.TotalRecovered
+        doughnut.cases = countryDetails.TotalConfirmed
+      } else {
+        // if canot find country exists, we get the latest case
+        const recentDetailed = this.getCountryDetailedCases[0]
+        if (recentDetailed) {
+          doughnut.active =
+            recentDetailed.cases.total -
+            (recentDetailed.deaths.total + recentDetailed.cases.recovered)
+
+          doughnut.deaths = recentDetailed.deaths.total
+          doughnut.recovered = recentDetailed.cases.recovered
+          doughnut.cases = recentDetailed.cases.total
+        }
+      }
+
+      this.options.title.text = `Total Number of confirmed cases: ${doughnut.cases}`
       this.data = {
         labels: ['Active', 'Death', 'Treated'],
         datasets: [
           {
             label: 'Data One',
             backgroundColor: ['#ffc107', '#dc3545', '#28a745'],
-            data: [active, death, treated]
+            data: [doughnut.active, doughnut.deaths, doughnut.recovered]
           }
         ]
       }
