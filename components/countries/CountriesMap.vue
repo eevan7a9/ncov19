493cf358151618d53a8a2cfb6049f6b3ed968cf8
eevan7a9 <e7a9@protonmail.com>
2020-11-15 06:31:57 +0900

    replace with correct countries geojson

--- components/countries/CountriesMap.vue
+++ components/countries/CountriesMap.vue
@@ -1,37 +1,58 @@
 <template lang="pug">
-  div#mapContainer.h-100.w-100
-    client-only
-      l-map(:zoom="map.zoom" :center="map.center")
-        l-tile-layer(:url="tile.url" :attribution="tile.attribution")
-        //- l-marker(v-for="(marker, index) in getCountriesCases" :key="index" :lat-lng="marker.latLang")
-        l-circle(
-          v-for="(marker, index) in getCountriesCases" :key="index"
-          :lat-lng="marker.latLang"  :color="circle.color"
-          :fillColor="circle.fillColor" :fillOpacity="circle.fillOpacity"
-          :radius="marker.radius * 2"
-           @add="$nextTick(()=> checkCountryIsTopFive($event.target, marker))"
-        )
-          l-popup(:options="{closeOnClick: false,autoClose: false}")
-            p.m-0.p-0.text-center.text-uppercase
-              strong {{marker.Country}}
-            p.m-0.p-0 Total Confirmed :
-              strong.text-info {{marker.TotalConfirmed}}
-            p.m-0.p-0 Total Deaths :
-              strong.text-danger {{marker.TotalDeaths}}
-            p.m-0.p-0 Total Recovered :
-              strong.text-success {{marker.TotalRecovered}}
-            p.text-center.text-uppercase.m-0.mt-1.p-0
-              router-link(:to="{ name: 'Cases-country', params: { country:marker.name }}").font-weight-bolder.text-primary
-                u More Details
-
+#mapContainer.h-100.w-100
+  client-only
+    l-map(:zoom='map.zoom', :center='map.center')
+      l-tile-layer(:url='tile.url', :attribution='tile.attribution')
+      l-choropleth-layer(
+        :data='getSummaryCases',
+        titleKey='country',
+        idKey='country',
+        :value='value',
+        :extraValues='extraValues',
+        geojsonIdKey='id',
+        :geojson='countriesGeojson',
+        :colorScale='colorScale'
+      )
+      //- l-marker(v-for="(marker, index) in getCountriesCases" :key="index" :lat-lng="marker.latLang")
+      //- l-circle(
+      //-   v-for="(marker, index) in getCountriesCases" :key="index"
+      //-   :lat-lng="marker.latLang"  :color="circle.color"
+      //-   :fillColor="circle.fillColor" :fillOpacity="circle.fillOpacity"
+      //-   :radius="marker.radius * 2"
+      //-    @add="$nextTick(()=> checkCountryIsTopFive($event.target, marker))"
+      //- )
+      //-   l-popup(:options="{closeOnClick: false,autoClose: false}")
+      //-     p.m-0.p-0.text-center.text-uppercase
+      //-       strong {{marker.Country}}
+      //-     p.m-0.p-0 Total Confirmed :
+      //-       strong.text-info {{marker.TotalConfirmed}}
+      //-     p.m-0.p-0 Total Deaths :
+      //-       strong.text-danger {{marker.TotalDeaths}}
+      //-     p.m-0.p-0 Total Recovered :
+      //-       strong.text-success {{marker.TotalRecovered}}
+      //-     p.text-center.text-uppercase.m-0.mt-1.p-0
+      //-       router-link(:to="{ name: 'Cases-country', params: { country:marker.name }}").font-weight-bolder.text-primary
+      //-         u More Details
 </template>
 
 <script>
+import countriesGeojson from 'assets/geojson/countries.json'
 import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'CountriesMap',
   data() {
     return {
+      value: {
+        key: 'amount_w',
+        metric: '% girls'
+      },
+      extraValues: [
+        {
+          key: 'amount_m',
+          metric: '% boys'
+        }
+      ],
+      colorScale: ['e7d090', 'e9ae7b', 'de7062'],
       map: {
         zoom: 4,
         center: [47.31322, -1.319482]
@@ -51,7 +72,10 @@ export default {
   },
   computed: {
     ...mapGetters(['getSummaryCases']),
-    ...mapGetters('countries', ['getCountriesCases'])
+    ...mapGetters('countries', ['getCountriesCases']),
+    countriesGeojson() {
+      return countriesGeojson
+    }
   },
   watch: {
     getSummaryCases() {
@@ -66,6 +90,8 @@ export default {
     this.$nextTick(() => {
       this.map.center = [47.31322, -1.319482]
     })
+    // eslint-disable-next-line no-console
+    console.log(countriesGeojson)
   },
   created() {},
   methods: {
