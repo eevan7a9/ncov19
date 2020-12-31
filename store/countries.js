3558d7733c96b97cf510a684cb05d7fd21ae9433
eevan7a9 <e7a9@protonmail.com>
2021-01-01 03:12:21 +0900

    perf(Countries): remove radius

--- store/countries.js
+++ store/countries.js
@@ -53,26 +53,7 @@ export const actions = {
             } else {
               country.name = foundCountry.name
             }
-            // we set radius to help visualize
-            if (country.TotalConfirmed > 300000) {
-              country.radius = 300000
-            } else if (country.TotalConfirmed > 100000) {
-              country.radius = 200000
-            } else if (country.TotalConfirmed > 50000) {
-              country.radius = 150000
-            } else if (country.TotalConfirmed > 25000) {
-              country.radius = 100000
-            } else if (country.TotalConfirmed > 10000) {
-              country.radius = 80000
-            } else if (country.TotalConfirmed > 5000) {
-              country.radius = 60000
-            } else if (country.TotalConfirmed > 1000) {
-              country.radius = 35000
-            } else if (country.TotalConfirmed > 500) {
-              country.radius = 20000
-            } else {
-              country.radius = 10000
-            }
+            country.radius = 600000
             countries.push(country)
           }
         }
