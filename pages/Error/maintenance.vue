f24184bc1e8017a554132ba80602ea2d3890897f
eevan7a9 <e7a9@protonmail.com>
2021-01-01 14:47:16 +0900

    perf(maintenance): added btn redirect

--- pages/Error/maintenance.vue
+++ pages/Error/maintenance.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   div.bg-light.d-flex.flex-column.justify-content-center.align-items-center
-    h1 Let's try again in...
+    h1 Sever is down...
+    h2(v-if="!showBtn") Let's try again in &#32;
       strong.text-danger {{timer}}
+    button.btn.btn-danger.p-3.mb-3.font-weight-bold(type='button' v-else @click="redirect") Try Again
     b-img(
       src="@/assets/illustration/undraw_clean_up_ucm0.svg"
       fluid
@@ -14,21 +16,23 @@ export default {
   layout: 'dashboard',
   data() {
     return {
-      timer: 15
+      timer: 15,
+      showBtn: false
     }
   },
+  watch: {},
   mounted() {
     const timerInit = setInterval(() => {
       this.timer = this.timer - 1
       if (this.timer === 0) {
         clearInterval(timerInit)
-        this.$router.push({ path: '/' })
+        this.showBtn = true
       }
     }, 1000)
   },
   methods: {
-    stopTimer(item) {
-      clearInterval(item)
+    redirect() {
+      this.$router.push({ path: '/' })
     }
   }
 }
