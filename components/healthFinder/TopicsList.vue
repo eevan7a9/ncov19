fe1c302c1ca05a24b9a57818aeeaf4a03d0fc4f3
eevan7a9 <e7a9@protonmail.com>
2021-01-08 01:32:58 +0900

    feat(Media): additional content from Health.gov

---
+++ components/healthFinder/TopicsList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title">Health and Wellness Topics</h4>
+        <div class="card-subtitle mb-2 text-muted">
+          Additional info from
+          <a href="https://health.gov/">health.gov </a>
+        </div>
+      </div>
+      <div class="card-body">
+        <ul class="list-group list-group-flush">
+          <li
+            v-for="topic of topics"
+            :key="topic.Id"
+            class="list-group-item"
+            @click="viewTopic(topic.Id)"
+          >
+            {{ topic.Title }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      topics: []
+    }
+  },
+  async fetch() {
+    const result = await axios.get(
+      `https://health.gov/myhealthfinder/api/v3/itemlist.json?Type=topic`
+    )
+    this.topics = result.data.Result.Items.Item
+  },
+  methods: {
+    viewTopic(topicId) {
+      console.log(topicId)
+      this.$router.push({ path: '/media/health/' + topicId })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+li {
+  cursor: pointer;
+  text-decoration: underline;
+}
+li:hover {
+  opacity: 0.8;
+  color: rgb(1, 143, 238);
+}
+</style>
--- pages/Media/index.vue
+++ pages/Media/index.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="articles" class="bg-light">
-    <div class="container">
-      <MediaLists />
+    <div class="container" style="background: white;">
+      <div class="row">
+        <MediaLists class="col-md-8 mt-3" />
+        <TopicsList class="col-md-4 mt-3" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import MediaLists from '@/components/cdc/MediaLists'
+import TopicsList from '@/components/healthFinder/TopicsList'
 export default {
   name: 'Articles',
   components: {
-    MediaLists
+    MediaLists,
+    TopicsList
   }
 }
 </script>
