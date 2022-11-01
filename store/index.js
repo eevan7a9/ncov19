2a5ff0f590a357c41df2ce13e15b4c5ac7012ef7
eevan7a9 <e7a9@protonmail.com>
2022-11-01 10:18:41 +0800

    perf(store): remove headers

--- store/index.js
+++ store/index.js
@@ -31,14 +31,8 @@ const getters = {
 const actions = {
   async fetchSummaryCases({ commit }) {
     try {
-      const result = await this.$axios.get(
-        'https://api.covid19api.com/summary',
-        {
-          headers: {
-            'X-Access-Token': '5cf9dfd5-3449-485e-b5ae-70a60e997864'
-          }
-        }
-      )
+      const result = await this.$axios.get('https://api.covid19api.com/summary')
+      console.log(result.data.Countries.length)
       const countries = result.data.Countries
       for (let i = 0; i < countries.length; i++) {
         const country = countries[i]
