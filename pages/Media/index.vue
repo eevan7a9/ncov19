94e7e6283f73822a2c48931ba0ce9dedaacfb787
eevan7a9 <e7a9@protonmail.com>
2023-09-27 14:58:57 +0800

    ref: base pages for v2

--- pages/Media/index.vue
+++ pages/Media/index.vue
@@ -1,28 +1,3 @@
 <template>
-  <div id="articles" class="bg-light">
-    <div class="container" style="background: white;">
-      <div class="row">
-        <MediaLists class="col-md-8 mt-3" />
-        <TopicsList class="col-md-4 mt-3" />
-      </div>
-    </div>
-  </div>
+    <h1>Media List</h1>
 </template>
-
-<script>
-import MediaLists from '@/components/cdc/MediaLists'
-import TopicsList from '@/components/healthFinder/TopicsList'
-export default {
-  name: 'Articles',
-  components: {
-    MediaLists,
-    TopicsList
-  }
-}
-</script>
-
-<style lang="scss" scoped>
-#articles {
-  min-height: 100vh;
-}
-</style>
