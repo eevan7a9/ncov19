4b5918eb810474445b20c09a05493034dda9fd4a
eevan7a9 <e7a9@protonmail.com>
2023-09-27 15:22:46 +0800

    build: added typescript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  typescript:{
+    typeCheck: true
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
