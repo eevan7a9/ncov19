26976ee211163f9e2579aa0506444d8c59f17523
eevan7a9 <e7a9@protonmail.com>
2023-09-30 23:47:43 +0800

    build: added Nuxt icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['nuxt-icon'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   typescript:{
