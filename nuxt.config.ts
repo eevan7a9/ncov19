9370710bd3c54d877880625d271beebbb3cd0354
eevan7a9 <e7a9@protonmail.com>
2023-10-02 15:54:35 +0800

    feat: server API for countries list

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    storage: {
+      myFileSystem: {
+        driver: 'fs',
+        base: 'assets/server/',
+      }
+    }
+  },
   modules: ['nuxt-icon'],
+  // disable dev-tools due to errors: https://github.com/nuxt/nuxt/issues/23493
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   typescript:{
