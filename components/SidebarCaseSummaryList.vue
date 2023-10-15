a2a6e47d03a50e1afea55b8c94dc3d4265232b0f
eevan7a9 <e7a9@protonmail.com>
2023-10-15 23:40:10 +0800

    style: update spacing & border

--- components/SidebarCaseSummaryList.vue
+++ components/SidebarCaseSummaryList.vue
@@ -3,21 +3,30 @@ import { storeToRefs } from 'pinia';
 
 const covidCasesStore = useCovidCasesStore()
 const { globalCases } = storeToRefs(covidCasesStore)
+
+onMounted(() => {
+    console.log(toRaw(globalCases.value))
+})
 </script>
 
 
 <template>
     <div class="h-full overflow-auto">
         <div class="flex flex-col gap-4 px-3 pt-6">
-            <div v-for="(country, i) of globalCases" :key="i" class="bg-white rounded-sm p-3 cursor-pointer hover:bg-gray-100">
-                <h1 class="text-xl font-bold mb-3">
+            <div v-for="(country, i) of globalCases" :key="i" class="bg-white cursor-pointer rounded-md hover:bg-gray-100">
+                <h1 class="text-xl font-bold mb-3 border-b-2 py-3 px-4">
                     {{ country.name }}
                     <small v-if="country.countryCode">
                         ({{ country.countryCode }})
                     </small>
                 </h1>
 
-                <ul class="flex flex-col  xl:flex-row xl:gap-5 mb-3">
+                <ul class="flex flex-col justify-between  xl:flex-row xl:gap-5 mb-3 px-4 py-3">
+                    <li class="flex flex-col">
+                        <span class=" font-semibold">Region</span>
+                        <span v-if="country.name !== 'Global'">{{ country.whoRegion }}</span>
+                        <span v-else>World</span>
+                    </li>
                     <li class="flex flex-col">
                         <span class=" font-semibold">Total Cases</span>
                         <span>{{ formatNumberWithCommas(country.casesCumulativeTotal) }}</span>
@@ -28,8 +37,8 @@ const { globalCases } = storeToRefs(covidCasesStore)
                     </li>
                 </ul>
 
-                <ul>
-                    <li class="flex flex-col">
+                <ul class=" px-4 border-t-2 py-3">
+                    <li class="flex flex-col mb-3">
                         <span class=" font-semibold">Cases Per Hundred Thousand</span>
                         <span>{{ formatNumberWithCommas(country.casesCumulativeTotalPer100000Population) }}</span>
                     </li>
