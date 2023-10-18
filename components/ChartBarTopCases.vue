34a9c07d2b23df4285e7f04cd0bdb7ee7e0d9943
eevan7a9 <e7a9@protonmail.com>
2023-10-19 05:41:30 +0800

    ref(colors): update colors

--- components/ChartBarTopCases.vue
+++ components/ChartBarTopCases.vue
@@ -2,7 +2,6 @@
 import { ChartDataset } from 'chart.js';
 import { storeToRefs } from 'pinia';
 
-const showBarChart = ref(false)
 const labels = ref<string[]>(["Cases"])
 const datasets = ref<ChartDataset<"bar">[]>([])
 
@@ -15,19 +14,23 @@ function scaleTickCallback(tickValue: number) {
 
 onMounted(() => {
 
+    const highestDeaths = topCountriesByCases.value[1]?.casesCumulativeTotal || 0
+
     datasets.value = topCountriesByCases.value.map((country) => (
         {
             label: country.countryCode,
-            data: [Number(country.casesCumulativeTotal)]
+            data: [Number(country.casesCumulativeTotal)],
+            backgroundColor: shadeColor(
+            Number(country?.casesCumulativeTotal),
+            Number(highestDeaths))
         }
     ))
-    showBarChart.value = true
 })
 </script>
 
 <template>
     <div class="w-full bg-white opacity-80 hover:opacity-100">
         <utils-charts-bar-chart :bar-data="{ labels, datasets }" :title="'Top Coutries by Case'"
-            :scale-tick-callback="scaleTickCallback" v-if="showBarChart" />
+            :scale-tick-callback="scaleTickCallback" v-if="datasets.length" />
     </div>
 </template>
--- utils/color.ts
+++ utils/color.ts
@@ -1,5 +1,6 @@
 export const shadeColor = function(currentTotalCase:number, highestTotalCase: number) {
     const percent = Math.ceil(Number(((currentTotalCase / highestTotalCase) * 100).toFixed(2))) 
+
     if (percent < 1) {
       return '#ffff1a'
     } else if (percent <= 2) {
@@ -11,15 +12,17 @@ export const shadeColor = function(currentTotalCase:number, highestTotalCase: nu
     } else if (percent <= 40) {
       return '#ff8566';
     } else if (percent <= 50) {
-        return '#e62e00'; 
+        return '#ff6600'; 
     } else if (percent <= 70) {
-        return '#ff0000'
+        return '#ff4000'
     } else if(percent <= 90) {
         return '#b30000'
     } else if (percent >= 91 && percent <= 100) {
-        return "#800000"
+        return "#e60000"
+    } else if (percent > 100) {
+        return '#990000'
     }
     else {
-      return '#000000'; // Default color (black) or handle other cases
+      return '#fff'; // Default color (black) or handle other cases
     }
 }
