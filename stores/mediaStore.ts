cacbab23caa49f20f6a29d90fd35dadcb72176b2
eevan7a9 <e7a9@protonmail.com>
2023-10-21 09:18:36 +0800

    typo(store): update state name

--- stores/mediaStore.ts
+++ stores/mediaStore.ts
@@ -3,14 +3,14 @@ import { defineStore } from "pinia";
 export const useMediaStore = defineStore('media', {
     state: () => ({
         mediaCdcList: [] as CDCMedia[],
-        aticlesHealthList: [] as HealthArticle[]
+        articlesHealthList: [] as HealthArticle[]
     }),
     actions: {
         setMediaCdcList(list: CDCMedia[]) {
             this.mediaCdcList = list
         },
         setArticlesHealth(articles: HealthArticle[]) {
-            this.aticlesHealthList = articles
+            this.articlesHealthList = articles
         }
     }
 }) 
