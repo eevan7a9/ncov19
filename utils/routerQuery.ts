3e5d01acf8c5b7f1a4b48312a8de6eab43594e3c
eevan7a9 <e7a9@protonmail.com>
2023-10-21 08:22:24 +0800

    fix(lint): type import

--- utils/routerQuery.ts
+++ utils/routerQuery.ts
@@ -1,4 +1,4 @@
-import { Router } from "#vue-router"
+import { type Router } from "#vue-router"
 
 export function alpha2RouteQuery(alpha2: string | undefined, router: Router) {
     const query = { alpha2 }
